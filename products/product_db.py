4d3d6a637a66fb05de0df8c3b8b26a335a23267e
Ben de Wet <bendwet@outlook.com>
2021-12-14 11:45:14 +1300

    first database structure complete

--- products/product_db.py
+++ products/product_db.py
@@ -20,4 +20,34 @@ db = SQLAlchemy(app)
 class Stores(db.Model):
     store_id: int = db.Column(db.Integer, primary_key=True)
     store_name: str = db.Column(db.String(40), unique=True, nullable=False)
+    store_id_relationships = db.relationship('StoreProducts', secondary='Prices', lazy=True)
+
+
+class Products(db.Model):
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(60), nullable=False)
+    unit_of_measure = db.Column(db.String(15), nullable=False)
+    unit_of_measure_size = db.Column(db.Float, nullable=False)
+    p_id_relationships = db.relationship('Prices', secondary='StoreProducts', lazy=True)
+
+
+class StoreProducts(db.Model):
+    store_product_id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.store_id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'), nullable=False)
+    store_product_code = db.Column(db.String(40), nullable=False)
+
+
+class Prices(db.Model):
+    price_id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.store_id'), nullable=False)
+    price_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    price = db.Column(db.Float, nullable=False)
+    is_onsale = db.Column(db.Boolean, default=False)
+    price_sale = db.Column(db.Float)
+
+
+# db.create_all()
+
 
