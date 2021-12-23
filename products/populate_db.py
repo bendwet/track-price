a9306002fbdaf383cc408b12ec3d5e335d3e9e07
Ben de Wet <bendwet@outlook.com>
2021-12-23 13:37:57 +1300

    Update populate_db.py

--- products/populate_db.py
+++ products/populate_db.py
@@ -1,36 +1,60 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from countdown_api.countdown_price_retriever import product_price
 from countdown_api.countdown_product_retriever import countdown_product
-from product_db import db, Stores, StoreProducts, Products, Prices
+from product_db import db, Stores, StoreProducts, Products
+from sqlalchemy import select
 
 TABLE_NAMES = ['stores', 'products', 'store_products', 'prices']
 
-# query all info from Stores table
-get_store = db.session.query(Stores)
-get_product = db.session.query(Products)
 
-# default value of countdown position in table
-countdown_position = 0
-product_position = 0
+class InsertProduct:
 
-# check which position countdown is in for retrieving correct store id.
-for row in get_store.all():
-    if row.store_name == 'countdown':
-        countdown_position = get_store.all().index(row)
+    new_store_id = 0
+    new_product_id = 0
 
-# check which position product name is in for retrieving product store id.
-for row in get_product.all():
-    if row.product_name == countdown_product.product_name:
-        product_position = get_product.all().index(row)
-        
-# countdown_position = get_store.all().index('countdown')
-# print(countdown_position)
+    def __init__(self, name_of_store, new_product_name, new_product_code, new_unit_of_measure, new_size):
+        self.name_of_store = name_of_store
+        self.new_product_name = new_product_name
+        self.new_product_code = new_product_code
+        self.new_unit_of_measure = new_unit_of_measure
+        self.new_size = new_size
 
-countdown_id = get_store.all()[countdown_position].store_id
-product_id = get_product.all()[product_position].product_id
+    def get_store(self):
+        """
+        Get store id from database corresponding to provided store name. If the store does not exist, create a new
+        store then get the new store id from the new store.
+        """
+        # select statement to get column where store name = countdown only
+        store = select(Stores).where(Stores.store_name == self.name_of_store)
+        # execute the select
+        result = db.session.execute(store)
+        # scalars() to return single element rather than row object, returns columns value instead of the column itself
+        store_id_result = result.scalars().all()[0].store_id
+        self.new_store_id = store_id_result
+        print(self.new_store_id)
 
-print(countdown_product)
+    def new_product(self):
+        """
+        Insert a new product into the product table with provided information as columns.
+        """
+        insert_new_product = Products(product_name=self.new_product_name, unit_of_measure=self.new_unit_of_measure,
+                                      unit_of_measure_size=self.new_size)
+        db.session.add(insert_new_product)
+        db.commit()
+        self.new_product_id = insert_new_product.product_id
+        print(self.new_product_id)
 
-print(countdown_id)
-print(product_id)
+    def new_store_product(self):
+        """
+        Insert a new store product
+        """
+        insert_new_store_product = StoreProducts(store_product_code=self.new_product_code, store_id=self.new_product_id,
+                                                 product_id=self.new_product_id)
+        db.session.add(insert_new_store_product)
+        db.commit()
+
+
+class InsertPrice:
+    pass
+
+
