63cca0f4841b87d267d92df84fac3b0e3cc238bf
Ben de Wet <bendwet@outlook.com>
2021-12-23 16:12:03 +1300

    price class updated

--- products/populate_db.py
+++ products/populate_db.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from countdown_api.countdown_product_retriever import countdown_product
-from product_db import db, Stores, StoreProducts, Products
+from product_db import db, Stores, StoreProducts, Products, Prices
 from sqlalchemy import select
 
 TABLE_NAMES = ['stores', 'products', 'store_products', 'prices']
@@ -53,8 +53,47 @@ class InsertProduct:
         db.session.add(insert_new_store_product)
         db.commit()
 
+    def product_run_all(self):
+        self.get_store()
+        self.new_product()
+        self.new_product()
+
 
 class InsertPrice:
-    pass
 
+    new_store_id = 0
+    new_product_id = 0
 
+    def __init__(self, new_price_date, new_price, new_is_onsale, new_price_sale, store_item_code):
+        self.new_price_date = new_price_date
+        self.new_product_price = new_price
+        self.new_is_onsale = new_is_onsale
+        self.new_price_sale = new_price_sale
+        self.store_item_code = store_item_code
+
+    def get_id(self):
+        """
+        Get product_id and price_id from store_products table, raise error if provided product code does not exist
+        in the table.
+        """
+        get_ids = select(StoreProducts.store_id, StoreProducts.product_id).where(StoreProducts.store_product_code ==
+                                                                                 self.store_item_code)
+        result = db.session.execute(get_ids)
+        id_result = result.all()[0]
+        self.new_store_id = id_result[0]
+        self.new_product_id = id_result[1]
+
+    def new_price(self):
+        """
+        Insert new price of product into price table.
+        """
+        insert_new_price = Prices(product_id=self.new_product_id, store_id=self.new_store_id,
+                                  price_date=self.new_price_date, price=self.new_product_price,
+                                  is_onsale=self.new_is_onsale)
+
+        db.session.add(insert_new_price)
+        db.session.commit()
+        
+    def price_run_all(self):
+        self.get_id()
+        self.new_price()
