9fd9979c0a224c76d830b21815ec2ad57afcad49
Ben de Wet <83267305+bendwet@users.noreply.github.com>
2022-01-17 10:13:47 +1300

    Update product_db.py

--- products/product_db.py
+++ products/product_db.py
@@ -13,8 +13,8 @@ COMPANY_PACKNSAVE = 'packnsave'
 # USER = os.environ['MYSQLUSER']
 # PASSWORD = os.environ['MYSQLPASSWORD']
 
-# ORCL_USER = os.environ['ORCLUSER']
-# ORCL_PASSWORD = os.environ['ORCLPASSWORD']
+ORCL_USER = os.environ['ORCLUSER']
+ORCL_PASSWORD = os.environ['ORCLPASSWORD']
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{USER}:{PASSWORD}@localhost/pricedb'
@@ -22,7 +22,7 @@ app = Flask(__name__)
 cx_Oracle.init_oracle_client(lib_dir="/app/oracle_lib/instantclient_21_4",
                              config_dir="/app/oracle_config/Wallet_pricedb")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'oracle+cx_oracle://admin:Silvermoshi1@pricedb_high'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'oracle+cx_oracle://{ORCL_USER}:{ORCL_PASSWORD}@pricedb_high'
 
 # SQLAlchemy database instance
 db = SQLAlchemy(app)
