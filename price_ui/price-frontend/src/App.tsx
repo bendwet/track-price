fe1bd5977bcdab85d53c0b22ad933d49396353c8
Ben de Wet <bendwet@outlook.com>
2022-01-25 16:40:13 +1300

    initial frontend commit

---
+++ price_ui/price-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Price Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
