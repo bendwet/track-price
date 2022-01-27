bcccacbcaf48bec0e5db91e7b3d93076d64c9d10
Ben de Wet <bendwet@outlook.com>
2022-01-27 15:16:23 +1300

    add Button to ItemBasket

---
+++ price_ui/price-frontend/src/components/AddItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AddItem() {
+  return (
+  <div>
+      <button>
+          Add Item
+      </button>
+  </div>
+  );
+}
+
+export default AddItem;
--- price_ui/price-frontend/src/pages/ItemBasket.tsx
+++ price_ui/price-frontend/src/pages/ItemBasket.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import '../css/ItemBasket.css'
 import BasketNavbar from "../components/BasketNavbar";
 import ItemList from "../components/ItemList";
+import AddItem from "../components/AddItem";
 
 function ItemBasket() {
     return (
         <div className="ItemBasket">
             <h1 className="ItemBasketText">ItemBasket</h1>
-            <div>
-                <BasketNavbar />
+            <div className="TopBarContainer">
+                <div className="AddItemButton">
+                    <AddItem />
+                </div>
+                <div className="BasketNavbar"> 
+                    <BasketNavbar />
+                </div>
             </div>
             <div>
                 <ItemList />
