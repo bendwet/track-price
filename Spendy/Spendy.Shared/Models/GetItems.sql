1d72e87c06900ffe2108054d243ab18d32e84a76
Ben de Wet <bendwet@outlook.com>
2022-07-06 15:00:03 +1200

    format sql queries

--- Spendy/Spendy.Shared/Models/GetItems.sql
+++ Spendy/Spendy.Shared/Models/GetItems.sql
@@ -9,46 +9,46 @@
             prices.product_id
     )
    , product_out_of_stock as (
-    SELECT
-        p.product_id
-    FROM
-        prices p
-            INNER JOIN last_date_per_product ldpp on ldpp.product_id = p.product_id
-            AND p.price_date = ldpp.max_date
-    GROUP BY
-        product_id
-    HAVING
-            MAX(p.price_sale) = 0
-)
+        SELECT
+            p.product_id
+        FROM
+            prices p
+                INNER JOIN last_date_per_product ldpp on ldpp.product_id = p.product_id
+                    AND p.price_date = ldpp.max_date
+        GROUP BY
+            product_id
+        HAVING
+                MAX(p.price_sale) = 0
+    )
    , lowest_price_per_product_day_ranked as (
-    SELECT
-        p.price_id
-         , p.product_id
-         , products.product_name
-         , p.price_sale
-         , p.price_quantity
-         , p.is_available
-         , p.is_onsale
-         , p.price_date
-         , RANK() OVER (PARTITION BY p.product_id ORDER BY p.price_sale ASC) as price_rank
+        SELECT
+            p.price_id
+             , p.product_id
+             , products.product_name
+             , p.price_sale
+             , p.price_quantity
+             , p.is_available
+             , p.is_onsale
+             , p.price_date
+             , RANK() OVER (PARTITION BY p.product_id ORDER BY p.price_sale ASC) as price_rank
     FROM
         prices p
             INNER JOIN last_date_per_product ldpp ON ldpp.product_id = p.product_id
-            AND p.price_date = ldpp.max_date
+                AND p.price_date = ldpp.max_date
             INNER JOIN products ON p.product_id = products.product_id
     WHERE
             p.is_available = true
 )
    , lowest_price_per_product_day as (
-    SELECT
-        *
-    FROM
-        lowest_price_per_product_day_ranked
-    WHERE
-            price_rank = 1
-    GROUP BY
-        product_id
-)
+        SELECT
+            *
+        FROM
+            lowest_price_per_product_day_ranked
+        WHERE
+                price_rank = 1
+        GROUP BY
+            product_id
+    )
 SELECT
     p.product_id
      , p.product_name
--- Spendy/Spendy.Shared/Models/GetItemsByProductId.sql
+++ Spendy/Spendy.Shared/Models/GetItemsByProductId.sql
@@ -7,29 +7,29 @@
         FROM
             prices p
         WHERE
-                p.product_id = @productId
+            p.product_id = @productId
         GROUP BY
             p.store_id
     )
    , product_price_per_store as (
-    SELECT
-        ldps.product_id
-         , ldps.store_id
-         , pr.product_name
-         , s.store_name
-         , p.price_sale
-         , p.is_available
-         , p.is_onsale
-         , p.price_quantity
-         , ldps.max_date as price_date
-    FROM
-        prices p
-            INNER JOIN last_date_for_product_per_store ldps ON ldps.product_id = p.product_id
-            AND ldps.max_date = p.price_date AND ldps.store_id = p.store_id
-            INNER JOIN products pr ON pr.product_id = p.product_id
-            INNER JOIN stores s ON s.store_id = p.store_id
+        SELECT
+            ldps.product_id
+             , ldps.store_id
+             , pr.product_name
+             , s.store_name
+             , p.price_sale
+             , p.is_available
+             , p.is_onsale
+             , p.price_quantity
+             , ldps.max_date as price_date
+        FROM
+            prices p
+                INNER JOIN last_date_for_product_per_store ldps ON ldps.product_id = p.product_id
+                    AND ldps.max_date = p.price_date AND ldps.store_id = p.store_id
+                INNER JOIN products pr ON pr.product_id = p.product_id
+                INNER JOIN stores s ON s.store_id = p.store_id
 
-)
+    )
 SELECT *
 FROM
     product_price_per_store
